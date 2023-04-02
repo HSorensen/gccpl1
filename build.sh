11cc0e024fdf37c5017a9ad7cbce342becab4de1
Henrik Sorensen <hsorensen@github.com>
2023-04-02 16:15:22 -0700

    docker: Build antlr static runtime and main.cpp (demo)

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build -f _docker/dockerfile -t itsme/gccpl1x .
 
-docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir itsme/gccpl1x
+docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir -v $PWD/src-antlr/src-parser/main.cpp:/src-antlr/src-parser/main.cpp itsme/gccpl1x
