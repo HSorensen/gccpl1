a364500c0724871dcdbca58ccf3f5789e16be746
Henrik Sorensen <hsorensen@github.com>
2023-04-02 23:19:48 -0700

    testall.sh: show returncode

--- src-antlr/src-parser/src-tests/testall.sh
+++ src-antlr/src-parser/src-tests/testall.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-GCCPLIBIN=../../run/usr/local/share/antlr4-demo
+#GCCPLIBIN=../../run/usr/local/share/antlr4-demo
+GCCPLIBIN=../../buildfolder/demo
 
 for  f in *.pl1; do
 
   echo $f 
-  echo "$f" >&2
   $GCCPLIBIN $f
 
   rc=$@
+  echo "$f: $rc" >&2
 
 done;
 
