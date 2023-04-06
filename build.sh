ee23d60135348797093a1df6c8fa100e20899e0a
Henrik Sorensen <hsorensen@github.com>
2023-04-06 04:30:49 -0700

    build.sh: list add g4 files to docker run

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 docker build -f _docker/dockerfile -t itsme/gccpl1x .
 
-docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir -v $PWD/src-antlr/src-parser/main.cpp:/src-antlr/src-parser/main.cpp itsme/gccpl1x
+docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir \
+  -v $PWD/src-antlr/src-parser/main.cpp:/src-antlr/src-parser/main.cpp \
+  -v $PWD/src-antlr/src-parser/Pl1Lexer.g4:/src-antlr/src-parser/Pl1Lexer.g4 \
+  -v $PWD/src-antlr/src-parser/Pl1Parser.g4:/src-antlr/src-parser/Pl1Parser.g4 \
+  itsme/gccpl1x
