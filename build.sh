161734024391aa12983ad26b826cc75ef1302b62
Henrik Sorensen <hsorensen@github.com>
2023-04-16 21:43:29 -0700

    build.sh: helper to start image

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 docker build -f _docker/dockerfile -t itsme/gccpl1x .
+if [ $? -ne 0 ]; then
+ echo "docker build failed. Check setup"; exit 8; 
+fi
 
-docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir \
-  -v $PWD/src-antlr/src-parser/main.cpp:/src-antlr/src-parser/main.cpp \
-  -v $PWD/src-antlr/src-parser/Pl1Lexer.g4:/src-antlr/src-parser/Pl1Lexer.g4 \
-  -v $PWD/src-antlr/src-parser/Pl1Parser.g4:/src-antlr/src-parser/Pl1Parser.g4 \
-  -v $PWD/src-antlr/src-parser/CMakeLists.txt:/src-antlr/src-parser/CMakeLists.txt \
-  itsme/gccpl1x
+#docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir \
+#  -v $PWD/src-antlr/src-parser/main.cpp:/src-antlr/src-parser/main.cpp \
+#  -v $PWD/src-antlr/src-parser/Pl1Lexer.g4:/src-antlr/src-parser/Pl1Lexer.g4 \
+#  -v $PWD/src-antlr/src-parser/Pl1Parser.g4:/src-antlr/src-parser/Pl1Parser.g4 \
+#  -v $PWD/src-antlr/src-parser/CMakeLists.txt:/src-antlr/src-parser/CMakeLists.txt \
+#  itsme/gccpl1x
+
+  # -v $PWD/src-gcc:/src-gcc \
+  # -v $PWD/src-gcc-gpli:/src-gcc/gcc/gpli \
+
+docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir -v $PWD/src-gcc/gcc:/src-gcc/gcc -v $PWD/src-gcc-gpli:/src-gcc/gcc/gpli itsme/gccpl1x
