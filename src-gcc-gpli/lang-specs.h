92919b85cdf32d0e4564d17e00ab18a39c21cc24
Henrik Sorensen <hsorensen@github.com>
2023-04-19 06:10:44 -0700

    lang-spec: add pli pl1 extentions

--- src-gcc-gpli/lang-specs.h
+++ src-gcc-gpli/lang-specs.h
@@ -1,4 +1,8 @@
-/* lang-specs.in */
+/* lang-specs.h */
 /* https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html */
 {".gpli",  "@gpli", 0, 1, 0},
+{".pli",  "@gpli", 0, 1, 0},
+{".pl1",  "@gpli", 0, 1, 0},
+{".PLI",  "@gpli", 0, 1, 0},
+{".PL1",  "@gpli", 0, 1, 0},
 {"@gpli",  "gpli1 %i %(cc1_options) %{!fsyntax-only:%(invoke_as)}", 0, 1, 0},
