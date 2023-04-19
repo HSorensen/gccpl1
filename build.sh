47f3a87b5ceb4e4d0ea94639810624164a0e23c0
Henrik Sorensen <hsorensen@github.com>
2023-04-18 23:15:37 -0700

    build.sh: add volume binds for src-gcc-gpli

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -f _docker/dockerfile -t itsme/gccpl1x .
+docker build -f _docker/dockerfile -t itsme/gccpl1 .
 if [ $? -ne 0 ]; then
  echo "docker build failed. Check setup"; exit 8; 
 fi
@@ -15,4 +15,4 @@ fi
   # -v $PWD/src-gcc:/src-gcc \
   # -v $PWD/src-gcc-gpli:/src-gcc/gcc/gpli \
 
-docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir -v $PWD/src-gcc/gcc:/src-gcc/gcc -v $PWD/src-gcc-gpli:/src-gcc/gcc/gpli itsme/gccpl1x
+docker run --rm --hostname gccpli -it -v $PWD/workdir:/workdir -v $PWD/src-gcc/gcc:/src-gcc/gcc -v $PWD/src-gcc-gpli:/src-gcc/gcc/gpli -v $PWD/src-gcc-tiny:/src-gcc/gcc/tiny itsme/gccpl1
