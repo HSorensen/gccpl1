91c00197fd5911d48a6c63e647fcd6c3d9b795de
hsorensen <hsorensen@github.com>
2023-05-28 13:22:44 -0700

    lang-specs.h: Pass I parameters to gpli1

--- src-gcc-gpli/lang-specs.h
+++ src-gcc-gpli/lang-specs.h
@@ -5,4 +5,4 @@
 {".pl1",  "@gpli", 0, 1, 0},
 {".PLI",  "@gpli", 0, 1, 0},
 {".PL1",  "@gpli", 0, 1, 0},
-{"@gpli",  "gpli1 %i %(cc1_options) %{!fsyntax-only:%(invoke_as)}", 0, 1, 0},
+{"@gpli",  "gpli1 %i %(cc1_options) %{I*} %{!fsyntax-only:%(invoke_as)}", 0, 1, 0},
