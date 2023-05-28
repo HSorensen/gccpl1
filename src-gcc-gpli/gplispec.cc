1c816936ba52f44cfd9d0c94350e3660881d5a33
hsorensen <hsorensen@github.com>
2023-05-28 13:21:42 -0700

    gplispec.cc: add output for clarity

--- src-gcc-gpli/gplispec.cc
+++ src-gcc-gpli/gplispec.cc
@@ -1,14 +1,25 @@
+
+#include <iostream>
+#include "config.h"
+#include "system.h"
+//#include "coretypes.h"
+//#include "tm.h"
+//#include "opts.h"
+
+
 void
-lang_specific_driver (struct cl_decoded_option ** /* in_decoded_options */,
-		      unsigned int * /* in_decoded_options_count */,
-		      int * /*in_added_libraries */)
-{
+lang_specific_driver (struct cl_decoded_option ** in_decoded_options,
+		      unsigned int * in_decoded_options_count,
+		      int * in_added_libraries )
+{ // Note only called if compiler is started with gccgpli command
+  fprintf (stderr, "gplispec.cc: lang_specific_driver in_decoded_options_count %i\n",*in_decoded_options_count);
 }
 
 /* Called before linking.  Returns 0 on success and -1 on failure.  */
 int
 lang_specific_pre_link (void)
-{
+{ // Note only called if compiler is started with gccgpli command
+  fprintf (stderr, "gplispec.cc: lang_specific_pre_link\n");
   /* Not used for Gpli.  */
   return 0;
 }
